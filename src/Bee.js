9217ee555c5611b03a33542dedfd154cb92923bd
Eden Mazzola <mazzolaeden@gmail.com>
2015-03-30 14:15:27 -0700

    "All the bees pass all the tests. They all inherit from thier previous bees"

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+	
+	Grub.call(this);
+	//in Grub.call, What is "this" referring to? is it the "this" in grub?
+	this.age = 5;
+	this.color = "yellow";
+	this.job = "keep on growing"
+	Bee.prototype.constructor = Bee;
 };
-
+//we need to tell the Bee that Grub is a Prototype of Bee. 
+//A failed lookup needs go up the prototype chain.
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
-var Grub = function() {
+var Grub = function(){
+	this.age = 0;
+	this.food = "jelly";
+	this.color = "pink";
+	
 };
+
+Grub.prototype.eat = function(){
+
+};
+
 
